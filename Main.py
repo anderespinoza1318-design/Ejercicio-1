b9cc253c0332156de250d02a7597223be36e0ce2
anderespinoza1318-design <anderespinoza1318@gmai.com>
2026-05-12 21:18:22 -0500

    feat: implementar bucle for para notas

--- Main.py
+++ Main.py
@@ -7,3 +7,10 @@ nota2 = float(input("Vale ahora Ingrese su segunda nota: "))
 nota3 = float(input("Ingrese su tercera nota: "))
 nota4 = float(input("Ingrese su cuarta nota: "))
 nota5 = float(input("Ingrese su quinta nota: "))
+
+suma = 0
+
+notas = [nota1, nota2, nota3, nota4, nota5]
+
+for nota in notas:
+    suma += nota
